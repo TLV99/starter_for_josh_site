e6700e7c5a4b9c7d4410b648f65566613dbf40b9
TLV99 <tlaverghetta@gmail.com>
2020-04-21 20:25:51 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -9,8 +9,6 @@
             <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
           <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
 <p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
-
-
    <div class="blocker"></div>
 </div>
 </section>
@@ -18,14 +16,28 @@
           <div class="conatiner">
           <div class="row">
               <div>
-              <h3 class="text-center article-title"> LATEST ARTICLES</h3>
+              <h3 class="text-center article-title "><?php the_field('titleforarticles'); ?></h3>
               </div>
-              <div class="col-md-4 article-cent">                     
+              
+              <div class="col-md-4 article-cent"> 
+<img class="img-responsive" alt="portait" src="<?php the_field('articleimage'); ?>">
+<h4><?php the_field('articletitle'); ?></h4>
+<p><?php the_field('articleexcerpt'); ?> </p>
+<a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a>
              </div>
- <div class="col-md-4 article-cent">                           
-  </div>
-  <div class="col-md-4 article-cent">            
+                            <div class="col-md-4 article-cent"> 
+<img class="img-responsive" alt="portait" src="<?php the_field('articleimagetwo'); ?>">
+<h4><?php the_field('articletitletwo'); ?></h4>
+<p><?php the_field('articleexcerpttwo'); ?> </p>
+<a href="<?php the_field('readmoretwo'); ?>" class="readmore">CONTINUE READING</a>
+             </div>              <div class="col-md-4 article-cent"> 
+<img class="img-responsive" alt="portait" src="<?php the_field('articleimagethree'); ?>">
+<h4><?php the_field('articletitlethree'); ?></h4>
+<p><?php the_field('articleexcerptthree'); ?> </p>
+<a href="<?php the_field('readmorethree'); ?>" class="readmore">CONTINUE READING</a>
              </div>
+
+  
               </div> 
           </div>
       </section>
