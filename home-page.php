737adf1c377f632e37e063b9d02a3d9a8b5c2dfb
TLV99 <tlaverghetta@gmail.com>
2020-04-21 19:45:52 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -5,7 +5,12 @@
     ?>
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
 <section class="container-fluid aboutbg text-center">    
-      <div class="container">         
+      <div class="container">      
+            <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
+          <p class="particle"><?php the_field('subtitleforaboutme'); ?></p>
+<p class="about-text"><?php the_field('paragraphforaboutme'); ?></p>
+
+
    <div class="blocker"></div>
 </div>
 </section>
